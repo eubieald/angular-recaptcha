7f0c014b1d348c702180056387dba58b9823db2d
Eubie Aluad <eubialua@mtechlabs.dev>
2025-10-23 23:09:24 +0800

    Initial Commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Recaptcha } from './components/recaptcha/recaptcha';
+import { RecaptchaModule } from 'ng-recaptcha';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Recaptcha, RecaptchaModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('angular-recaptcha');
